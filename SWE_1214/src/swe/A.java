efaee59139d9cb065d635f4fb7250bbd9ef19b5f
seokchan816 <channy0816@gmail.com>
2025-12-14 20:35:56 +0900

    앨리스 브런치에서 A,C 수정

--- SWE_1214/src/swe/A.java
+++ SWE_1214/src/swe/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("이것은 A프로그램입니다.");
+		System.out.print("앨리스 브런치.");
 	}
 }
--- SWE_1214/src/swe/C.java
+++ SWE_1214/src/swe/C.java
@@ -4,5 +4,6 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("이것은 자바 프로그램입니다.");
+		System.out.print("앨리스 브런치.");
 	}
 }
