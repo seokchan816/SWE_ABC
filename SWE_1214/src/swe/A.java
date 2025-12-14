eb3b72417e0064f4a0529dd7c7a409b56e0c6b4f
seokchan816 <channy0816@gmail.com>
2025-12-14 20:37:51 +0900

    밥에서 A,C 수정

--- SWE_1214/src/swe/A.java
+++ SWE_1214/src/swe/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("이것은 A프로그램입니다.");
+		System.out.print("흰쌀밥 입니다.");
 	}
 }
--- SWE_1214/src/swe/B.java
+++ SWE_1214/src/swe/B.java
@@ -4,5 +4,6 @@ public class B {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("이것은 B프로그램입니다.");
+		System.out.print("잡곡밥 입니다.");
 	}
 }
